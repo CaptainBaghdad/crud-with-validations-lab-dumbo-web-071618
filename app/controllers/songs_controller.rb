cdc4152c49bd8fddfae19b4e092d65bef12359bc
Jason Pitts <juniorjr083@gmail.com>
2018-08-17 00:33:11 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,38 +1,52 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :destroy, :update]
   def index
-    @song = Song.all
-  end 
-  
-  def show 
-    @song = Song.find(params[:id])
-  end 
-  
-  
+    @songs = Song.all
+  end
+
+  def show
+  end
+
   def new
     @song = Song.new
-    
-  end 
-  
-  def create
-    @song = Song.find(params[:id])
-    @song.title = params[:title]
-    @song.released = params[:released]
-    @song.release_year = params[:release_year]
-    @artist_name = params[:artist_name]
-    @genre = params[:genre]
-    
-  end 
-  
-  def edit 
-    
-    
   end
-  
+
+  def create
+    @song = Song.new(song_params(:title, :released, :release_year, :artist_name, :genre))
+
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+  end
+
   def update
-    
-  end 
-  
-  def delete
-    
-  end 
+    if @song.update(song_params(:title, :released, :release_year, :artist_name, :genre))
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @song.destroy
+    redirect_to songs_path
+  end
+
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
 end
